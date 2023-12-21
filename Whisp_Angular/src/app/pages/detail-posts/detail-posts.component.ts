89d4d279b4989d5c71326b4add2da4817ccbc1c6
Vasco_Faria <vascomfaria@ua.pt>
2023-12-21 20:28:10 +0000

    initiliaze components

---
+++ Whisp_Angular/src/app/pages/detail-posts/detail-posts.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-detail-posts',
+  standalone: true,
+  imports: [],
+  templateUrl: './detail-posts.component.html',
+  styleUrl: './detail-posts.component.css'
+})
+export class DetailPostsComponent {
+
+}
