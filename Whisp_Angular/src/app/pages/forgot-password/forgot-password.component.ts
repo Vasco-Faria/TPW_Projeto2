ae9317306e0d4a227c73d7c23b56716731c835e4
rodrigograc4 <rodrigomgraca@ua.pt>
2023-12-22 16:44:35 +0000

    routerlinks

--- Whisp_Angular/src/app/pages/forgot-password/forgot-password.component.ts
+++ Whisp_Angular/src/app/pages/forgot-password/forgot-password.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { AuthService } from '../../services/auth.service'; // Replace with the actual path to your authentication service
 import { ForgotPasswordModule } from './forgot-password.module';
+import { Router } from '@angular/router'; // Replace with the actual path to your router
 
 @Component({
   selector: 'app-forgot-password',
@@ -11,7 +12,7 @@ import { ForgotPasswordModule } from './forgot-password.module';
 export class ForgotPasswordComponent {
   changePasswordForm: FormGroup;
 
-  constructor(private fb: FormBuilder, private authService: AuthService) {
+  constructor(private fb: FormBuilder, private authService: AuthService, private router: Router) {
     this.changePasswordForm = this.fb.group({
       username: ['', Validators.required],
       email: ['', [Validators.required, Validators.email]],
@@ -31,9 +32,11 @@ export class ForgotPasswordComponent {
       this.authService.changePassword(passwordInfo).subscribe(
         (response) => {
           console.log('Password change successful', response);
+          this.router.navigate(['/login']);
         },
         (error) => {
           console.error('Password change error', error);
+          this.router.navigate(['/forgot-password']);
         }
       );
     }
